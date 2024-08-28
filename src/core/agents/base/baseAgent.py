8a260fc0bf989d4ed094f06c44f8b2fc87dfc236
everettVT <everett@vangelis.tech>
2024-08-28 13:27:26 -0700

    updated base agent

--- src/core/agents/base/baseAgent.py
+++ src/core/agents/base/baseAgent.py
@@ -1,14 +1,13 @@
 from enum import Enum
-
+from typing import List, Optional
 import daft
-import pyarrow as pa
-from maf.core.schema.base import CustomModel
 from pydantic import Field
 from ray import serve
 from ray.serve.handle import DeploymentHandle, DeploymentResponse
 
-from maf.core.components.tools import BaseTool
-
+from src.core.tools.tool import Tool
+from src.core.models.language.base import LLM
+from src.core.data.domain.base.domain_object import DomainObject
 
 class AgentStatus(str, Enum):
     IDLE = "idle"
@@ -18,39 +17,50 @@ class AgentStatus(str, Enum):
 
 
 @serve.deployment
-class mmAgent(CustomModel):
-    name: Optional[str] = Field(default_factory=RandomName(1)),
-    status: AgentStatus = "Idle"
-    mmllm: Optional[BaselLLM] = Field(default=MultiModalLLM(), description="The Multimodal Model"), no
-    reasoning: Optional[DeploymentHandle] = Field(default=MultiModalLLM(), description="The embedding model"),
-    memory: Optional[DeploymentHandle] = Field(
-        default=VectorMemory(
-            db=LanceDB(
-
-            ),
-            english=,
-            multilingual=,
-            multimodal=,
-            transcription=,
-            diarization=,
+class Agent(DomainObject):
+    """
+    The base agent class.
+    """
+    def __init__(self):
+        name: Optional[str] = Field(
+            default_factory=f"Agent {self.id}",
+            description="The name of the agent", 
         ),
-        description="The embedding model", ),
-    embedding: Optional[DeploymentHandle] = Field(default=MultiModalEmbedding(), description="The embedding model"),
-    speech:
-            logs: daft.DataType.embedding
-            metrics:
-            tools: Optional[pa.list_[BaseTool]]
+        status: AgentStatus = Field(
+            default="Idle",
+            description="The status of the agent"
+        ),
+        llm: Optional[LLM] = Field(
+            default= MMLLM(),
+            description="The Multimodal Model"
+        ),
+        reasoning: Optional[DeploymentHandle] = Field(
+            default=MultiModalLLM(),
+            description="The embedding model"
+        ),
+        memory: Optional[DeploymentHandle] = Field(
+            ...,
+            description="The embedding model",
+        ),
+        embedding: Optional[DeploymentHandle] = Field(
+            default=MultiModalEmbedding(),
+            description="The embedding model"
+        ),
+        tools: Optional[List[BaseTool]] = Field(
+            default=None,
+            description="The tools that the agent can use"
+        ),
     ):
-    super().(__init__(metadata=))
-        self.name =  str
-    self.models = model
-    self.
-    self.embedding = embedding
-    self.tools = pa.list_()
-    self.memory = self.get_query_pipelines()
+        super().__init__(metadata=)
+        self.name =  name
+        self.models = models
+        self.reasoning = reasoning
+        self.embedding = embedding
+        self.tools = tools
+        self.memory = 
 
-    self.settings.MAX_NUM_PENDING_TASKS = 100
-    self.settings.MAX_CONCURRENT_TASKS = 10
+        self.settings.MAX_NUM_PENDING_TASKS = 100
+        self.settings.MAX_CONCURRENT_TASKS = 10
 
 
 def __call__(self, request: Request, task: Task):
@@ -63,7 +73,7 @@ def __call__(self, request: Request, task: Task):
     query = get_query(prompt, strategies)
     get_context()
 
-    model_response: DeploymentResponse = self.model.remote(prompt)
+    model_response: DeploymentResponse = self.llm.remote(prompt)
     # Pass the adder response directly into the multipler (no `await` needed).
     multiplier_response: DeploymentResponse = self._multiplier.remote(
         adder_response
@@ -74,22 +84,12 @@ def __call__(self, request: Request, task: Task):
         unity.tables('data.agents')
 
 
-def refresh(self):
-    unity = get_unity()
-    ray = get_ray()
-    daft = get_daft()
+    def execute_task(self, Task) -> Result:
+        self.status = "Running"
+        self.status = "Running"
+        self.logger.info(f"{timestamp.utc(now)}Agent {self.name} started task {Task.name} [{Task.id}]")
 
-    def process(self):
-
-
-def execute_task(self, Task) -> Result,
-
-
-Action:
-self.status = "Running"
-self.logger.info(f"{timestamp.utc(now)}Agent {self.name} started task {Task.name} [{Task.id}]")
-
-return result
+        return result
 
 
 def get_status(self) -> AgentStatus:
